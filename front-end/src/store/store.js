5616e2d357904d68c0d46d880cb5f3560693aaba
alexmorales26 <alexmorales@csum.edu>
2019-02-27 23:01:29 -0800

    add missing dependencies; added changes to the store;

--- front-end/src/store/store.js
+++ front-end/src/store/store.js
@@ -1,24 +1,21 @@
 /* Implemented by Alexander Morales 2/27/19 */ 
-
 /* Documentation: [React-Router-redux V4] https://github.com/kurt-hardy/react-router-redux-issue/blob/master/src/configureStore.js */
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import { connectRouter, routerMiddleware } from 'connected-react-router'
 import { createLogger } from 'redux-logger'
+import history from './history';
 /* Documentation: [Redux-Persist] https://github.com/rt2zz/redux-persist#readme */
 /*import {persistStore,persistReducer} from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 */
 /* Imported component reducers */
-
+import {headerReducer} from '../components/common/header';
 const rootReducer = combineReducers(
     {
         router: connectRouter(history),
-        dashboard: ClaimsAuditReducer,
-        claimsInformation: ClaimsAuditCasualtyReducer,
-        selectAudit: CASelectAuditReducer,
-        auditPage:claimsAuditPageReducer
+         dashboard: headerReducer
     }
 );
 

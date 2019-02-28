3df6fbdec8d00762543c5715ac2dee684b994eee
alexmorales26 <alexmorales@csum.edu>
2019-02-27 20:22:15 -0800

    Redux store implementation and History Implementation

---
+++ front-end/src/store/store.js
@@ -0,0 +1,48 @@
+/* Implemented by Alexander Morales 2/27/19 */ 
+
+/* Documentation: [React-Router-redux V4] https://github.com/kurt-hardy/react-router-redux-issue/blob/master/src/configureStore.js */
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
+import { connectRouter, routerMiddleware } from 'connected-react-router'
+import { createLogger } from 'redux-logger'
+/* Documentation: [Redux-Persist] https://github.com/rt2zz/redux-persist#readme */
+/*import {persistStore,persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+*/
+/* Imported component reducers */
+
+const rootReducer = combineReducers(
+    {
+        router: connectRouter(history),
+        dashboard: ClaimsAuditReducer,
+        claimsInformation: ClaimsAuditCasualtyReducer,
+        selectAudit: CASelectAuditReducer,
+        auditPage:claimsAuditPageReducer
+    }
+);
+
+const middleware = [thunk];
+const composeEnhancers = [];
+
+/* Checking if Developer has Redux extension tools */
+var redux_tools =  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__(): compose
+
+if (process.env.NODE_ENV === "development") {
+  composeEnhancers.push(redux_tools);
+  middleware.push(createLogger())
+  middleware.push(routerMiddleware(history))
+}
+/*
+const persistedReducer = persistReducer(persistConfig,rootReducer);
+ */
+export const Store = createStore(
+    //connectRouter(history)(persistedReducer),
+    connectRouter(history)(rootReducer),
+    {},
+    compose(
+        applyMiddleware(...middleware),
+        ...composeEnhancers
+    )
+);
+//export const persistorStore = persistStore(Store);

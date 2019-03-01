a0fb47288e3e25b504f82b4796d3890b4a845fcf
mandamurillo <manda-murillo@csumb.edu>
2019-02-28 20:36:45 -0800

    styling and adding the navbar

--- front-end/src/components/common/header/UI/header.js
+++ front-end/src/components/common/header/UI/header.js
@@ -1,76 +1,97 @@
 import React from 'react';
 import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
 
 export default class Header extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false,
-      user: props.user
-    };
-  }
-  componentWillReceiveProps(nextProps){
-    this.setState({
-      user: nextProps.user
-    })
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  render() {
-    const {user} = this.state;
-    return (
-      <div>
-        <Navbar color="dark" dark expand="md">
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false,
+            user: props.user
+        };
+    }
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            user: nextProps.user
+        })
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        const { user } = this.state;
+        return ( <div>
+        <Navbar color="primary" dark expand="md">
           <NavbarBrand href="/home">CIGVue</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
+
+            <Nav className="mr-auto" navbar>
               <NavItem>
-                <NavLink href="/components/">Components</NavLink>
+                <NavLink href="/startOver">StartOver</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                <NavLink href="/filters">Filters</NavLink>
               </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
               <NavItem>
-               <h5 className="display-5"> {user} </h5>
+                <NavLink href="/Graphs">Graphs</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/exports">Exports</NavLink>
+              </NavItem>
+
+
+              <NavItem>
+
               </NavItem>
             </Nav>
-          </Collapse>
+          
         </Navbar>
       </div>
     );
   }
 }
+
+/*
+<div>
+<Navbar color="primary" dark expand="md">
+ <NavbarBrand href="/home">CIGVue</NavbarBrand>
+ //<NavbarToggler onClick={this.toggle} />
+ //<Collapse isOpen={this.state.isOpen} navbar>
+   <Nav className="mr-auto" navbar>
+     <NavItem>
+       <NavLink href="/startOver">StartOver</NavLink>
+     </NavItem>
+     <NavItem>
+       <NavLink href="/filters">Filters</NavLink>
+     </NavItem>
+     <NavItem>
+       <NavLink href="/Graphs">Graphs</NavLink>
+     </NavItem>
+     <NavItem>
+       <NavLink href="/exports">Exports</NavLink>
+     </NavItem>
+
+
+     <NavItem>
+
+      //<h5 className="display-5"> {user} </h5>
+     </NavItem>
+   </Nav>
+ </Collapse>
+</Navbar>
+</div>
+*/

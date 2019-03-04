7a33f0b30d315f12a3d7882a6cc291d08fecc1bc
evelinm <mandamurillo910@gmail.com>
2019-03-04 14:47:28 -0800

    added a button and added a file upload, I also made sure it stores the picture

--- front-end/src/routes/startover/containers/startover.js
+++ front-end/src/routes/startover/containers/startover.js
@@ -1,12 +1,31 @@
 import React,{Component} from 'react';
 
 
+const fileReader = new FileReader();
+fileReader.onload = handleFile;
+
+const  test_click = () => (
+ console.log('click')
+);
+
+const handleFile = (e) => {
+  const content = fileReader.result;
+  console.log('file content',  content)
+  // You can set content in state and show it in render.
+}
+
+const handleChangeFile = (file) => {
+  fileReader.readAsText(file);
+}
 export default class StartOver extends Component{
     render(){
         return (
               <div>
-              startovers
-          
+            <button  onClick={test_click} type="button">Click Me!</button>
+            <input onChange={(e)=>handleChangeFile(e.target.files[0])}type="file" name="myFile"/>
+
+
+
               </div>
         );
     }

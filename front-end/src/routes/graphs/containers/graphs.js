166bcc16877b57e1db02533528f7a3c617832dee
evelinm <mandamurillo910@gmail.com>
2019-03-03 20:55:10 -0800

    cleaned code, and added some cols and rows and flex, also added some styling

--- front-end/src/routes/graphs/containers/graphs.js
+++ front-end/src/routes/graphs/containers/graphs.js
@@ -14,39 +14,24 @@ export default class Graphs extends Component{
         return (
 
   <Container>
-
-
-
-
-<Row>
-   <Col xs={6} md={4}>
-   <GraphItem pic={ 'https://images.vexels.com/media/users/3/131136/isolated/preview/4c711c7ec7a01da4a8adf53684a13209-increasing-multicolor-line-chart-by-vexels.png'} />
+  <Row >
+   <Col className="firstGrid" >
+   <GraphItem pic={ 'https://4.bp.blogspot.com/_Bf48JKOl5HQ/TTRNfpBbXRI/AAAAAAAAB6U/nKKQZzThJEw/s1600/BlackBox.jpg'}fluid />
 
    </Col>
-   <Col xs={4} md={2}>
+
+   <Col className="SecondGrid">
+   <GraphItem pic={'https://4.bp.blogspot.com/_Bf48JKOl5HQ/TTRNfpBbXRI/AAAAAAAAB6U/nKKQZzThJEw/s1600/BlackBox.jpg'} />
 
    </Col>
-   <Col xs={6} md={4}>
-   <GraphItem pic={'https://images.vexels.com/media/users/3/131136/isolated/preview/4c711c7ec7a01da4a8adf53684a13209-increasing-multicolor-line-chart-by-vexels.png'} />
+  </Row>
 
-   </Col>
- </Row>
-
-
-
-<Row>
-
-
-<Col  xs={6} md={4}>
-<GraphItem pic={'https://images.vexels.com/media/users/3/131136/isolated/preview/4c711c7ec7a01da4a8adf53684a13209-increasing-multicolor-line-chart-by-vexels.png'} rounded/>
-</Col>
-<Col xs={4} md={2}>
-
-</Col>
-<Col  xs={6} md={4}>
-<GraphItem pic={'https://4.bp.blogspot.com/_Bf48JKOl5HQ/TTRNfpBbXRI/AAAAAAAAB6U/nKKQZzThJEw/s1600/BlackBox.jpg'}rounded />
-
-
+ <Row>
+ <Col className="thirdGrid"  >
+ <GraphItem pic={'https://4.bp.blogspot.com/_Bf48JKOl5HQ/TTRNfpBbXRI/AAAAAAAAB6U/nKKQZzThJEw/s1600/BlackBox.jpg'} roundedCircle />
+ </Col>
+ <Col className="fourthGrid"  >
+ <GraphItem pic={'https://4.bp.blogspot.com/_Bf48JKOl5HQ/TTRNfpBbXRI/AAAAAAAAB6U/nKKQZzThJEw/s1600/BlackBox.jpg'}rounded />
   </Col>
 </Row>
 

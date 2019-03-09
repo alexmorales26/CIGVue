7ceeb599ef2b52786eca1753fa8a4ea995e74e00
alexmorales26 <alexmorales@csum.edu>
2019-03-09 00:25:32 -0800

    removed the middleware that was being used to hook-up a package not in use no more

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fileUpload = require('express-fileupload');
 const routes = require('./routes/routes');
 const headers = require('./middlewares/headers');
 const app = express();
@@ -8,7 +7,7 @@ const hostName = process.env.HostName || 'localhost';
 
 app.use(headers);
 app.use('/',routes);
-app.use(fileUpload);
+
 
 app.listen(PORT, hostName,()=>{
    console.log(`Server listening on http://${hostName}:${PORT}`) 

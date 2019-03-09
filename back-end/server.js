b5d5e301dcbe25285ebfefbddded0729c8998201
Alexander Morales <Alexander@Alexanders-MacBook-Pro-2.local>
2019-03-08 23:56:20 -0800

    added some headers for the CORS; also the controller to recieve the data from the back-end; added some imports for the recent node module imported

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const routes = require('./routes/routes');
+const headers = require('./middlewares/headers');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const hostName = process.env.HostName || 'localhost';
 
+app.use(headers);
 app.use('/',routes);
+app.use(fileUpload);
 
 app.listen(PORT, hostName,()=>{
    console.log(`Server listening on http://${hostName}:${PORT}`) 

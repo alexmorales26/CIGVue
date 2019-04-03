43b2ad257fcd75f9a1b3aa6efacc71bc809e9d5a
Alexander Morales <Alexander@Alexanders-MacBook-Pro-2.local>
2019-04-02 23:12:16 -0700

    updated controller and removed logic from controller

--- back-end/controllers/recieveFileController/controller.js
+++ back-end/controllers/recieveFileController/controller.js
@@ -1,18 +1,7 @@
+const dataProcessor = require('../../util/dataProcessor/processor');
+
 const recieveFileController = (req, res) => {
-  console.log(req.files)
-  let uploadFile = req.files.file
-  const fileName = req.files.file.name
-  console.log(fileName);
-  uploadFile.mv(
-    `${__dirname}/public/files/${fileName}`,
-    function (err) {
-      if (err) {
-        return res.status(500).send(err)
-      }
-      res.json({
-        file: `public/${req.files.file.name}`,
-      })
-    },
-  )
+  let data = dataProcessor(req.body.data.file);
+  res.status(200).send(data);
 }
 module.exports = recieveFileController;

8adf053f3f2a60365427faf119ad625a204e0fa9
Nicco_E <nengbeck@csumb.edu>
2019-04-03 23:19:36 -0700

    fixed temp Dashboard Title
    It was bugging me

--- front-end/src/routes/dashboard/containers/dashboard.js
+++ front-end/src/routes/dashboard/containers/dashboard.js
@@ -1,14 +1,21 @@
 import React,{Component} from 'react';
-
+import '../../../assets/css/dashboard.css'
 
 export default class Dashboard extends Component{
     render(){
         return (
-              <div>  
+              <div>
+
+            <div id="body">
 
             <h1>Dashboard</h1>
 
-              </div>
+            </div>
+
+        
+        </div>
+
+
         );
     }
 }

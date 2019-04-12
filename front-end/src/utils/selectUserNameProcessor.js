eb72dd32d12aa46ab4b513bacac32a3ff23dee8f
Alexander Morales <Alexander@Alexanders-MacBook-Pro-2.local>
2019-04-11 19:10:26 -0700

    added logic to remove duplicate objects/names from the array of objects

--- front-end/src/utils/selectUserNameProcessor.js
+++ front-end/src/utils/selectUserNameProcessor.js
@@ -1,6 +1,30 @@
 const userNameProcessor = (data) => {
-  data.map((index) => {
-    console.log(`ITS DADDY :: ${index}`)
-  })
+  //console.log(data);
+  let options = [];
+  let item = {
+    value: "",
+    label: ""
+  }
+  if (data.length === 0 || data === undefined) {
+    return []
+  }
+  else {
+    data.map((index) => {
+      var x = Object.assign({}, item, {
+        value: index.Username,
+        label: index.Full_name
+      })
+      options.push(x);
+    })
+    const seen = new Set();
+    return options.filter(el => {
+      if (el.value !== undefined) {
+        const duplicate = seen.has(el.value);
+        seen.add(el.value);
+        return !duplicate;
+      }
+    });
+
+  }
 }
-export default userNameProcessor;
+export default { userNameProcessor };

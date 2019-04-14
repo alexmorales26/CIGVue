262a5e952c0708d8fcf96c2c103a3e9b6270134c
Nicholas Harding <nharding@csumb.edu>
2019-04-14 16:30:18 -0700

    Added basic code to interpret the array given in the file into a data array that contains only the information needed to diaplay the graphs.

--- front-end/src/components/dashboard/UI/stackedGraph/mixedBarPlot.js
+++ front-end/src/components/dashboard/UI/stackedGraph/mixedBarPlot.js
@@ -2,32 +2,88 @@ import React,{Component} from 'react';
 import {BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 
 const data = [
-  {
-    name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-  },
+  // {
+  //   name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+  // },
+  // {
+  //   name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+  // },
+  // {
+  //   name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+  // },
+  // {
+  //   name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+  // },
+  // {
+  //   name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+  // },
+  // {
+  //   name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+  // },
+  // {
+  //   name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+  // },
 ];
-
+const Bars = ({data}) => {
+  // data //iterate
+  // map
+    // return (
+    //   <Bar dataKey={}>
+    // )
+}
 export default class MixedBarPlot extends Component {
 
+  makeBarArray(in_data){
+    var inLen, i;
+    inLen = in_data.length;
 
+    //Going over each array in the array to find the three specific variables we need.
+    //We start at 1 because 0 is the header, which we don't want in the data.
+    for(i = 1; i < inLen; i++) {
+      //We'll need the name, the activity name, and the hours repeatedly in
+      //this loop, so we'll make variables for them here for ease of use.
+      var na, act, hrs;
+      na = in_data[i][5];
+      act = in_data[i][12];
+      hrs = in_data[i][2];
+
+      //Check to see if the name listed is in our data array first.
+      if(!data.some(d => d.name === na)) {
+        //If this name doesn't exist in our array, then we need to enter
+        //it into the array.
+        var tObj = new Object;
+
+        //Each object will store an array listing what the names of its own
+        //properties are, since they're dynamic and need a dynamic access.
+        var tArr = [act];
+        tObj.name = na;
+        tObj.act_types = tArr;
+
+        //This stores the hours spent on an activity in a property of the
+        //object that shares the activity's name.
+        tObj[tArr[0]] = hrs;
+        data.push(tObj);
+      } else {
+        //If the name does exist in our array, we just need to update that
+        //specific object.
+        //First we need to know which object we're updating.
+        var ind = data.findIndex(d => d.name === na);
+
+        //Next, we need to check if the name in our array already has a
+        //property with the current activity.
+        if(!data[ind].act_types.some(act)) {
+          //If it doesn't have a property with the current activity's name,
+          //add it to the list and as a property.
+          data[ind].act_types.push(act);
+          data[ind][act] = hrs;
+        } else {
+          //If a property with that activity's name already exists, we just
+          //need to find it and add the number of hours.
+          data[ind][act] += hrs;
+        }
+      }
+    }
+  }
 
   render() {
     return (
@@ -47,6 +103,7 @@ export default class MixedBarPlot extends Component {
         <Bar dataKey="pv" stackId="a" fill="#8884d8" />
         <Bar dataKey="amt" stackId="a" fill="#82ca9d" />
         <Bar dataKey="uv" fill="#ffc658" />
+        // <Bars data{this.state.data}/>
       </BarChart>
     );
   }
